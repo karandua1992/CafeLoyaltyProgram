5a1ec8f8600ac955d45f7b6ab18947d6c3ecd1ab
Surya Dua <karandua1992@gmail.com>
2023-04-05 14:48:11 +0100

    Support added for Hystrix fallback

--- cafeapp-api-gateway/src/main/java/ie/tcd/cafeapp/CafeAppApiGateway.java
+++ cafeapp-api-gateway/src/main/java/ie/tcd/cafeapp/CafeAppApiGateway.java
@@ -2,8 +2,10 @@ package ie.tcd.cafeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class CafeAppApiGateway {
 
 	public static void main(String[] args) {
